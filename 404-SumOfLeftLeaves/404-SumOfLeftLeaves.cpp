61907ea995202dee83ac9e387c4a35d1e0907932
NishadDamale <nishaddamale1010@gmail.com>
2026-06-26 13:42:58 +0530

    Create 404-SumOfLeftLeaves

---
+++ 404-SumOfLeftLeaves/404-SumOfLeftLeaves.cpp
@@ -0,0 +1,25 @@
+// Last updated: 6/26/2026, 1:43:00 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        if(!root) return 0;
+        if(root->left && !root->left->left && !root->left->right){
+            sum+=root->left->val;
+        }
+        sum+=sumOfLeftLeaves(root->left);
+        sum+=sumOfLeftLeaves(root->right);
+        return sum;
+    }
+};
