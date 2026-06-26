475f23f639a5fe50452133a0ae55a7452e3e725b
NishadDamale <nishaddamale1010@gmail.com>
2026-06-26 13:42:09 +0530

    Create 937-OnlineStockSpan

---
+++ 937-OnlineStockSpan/937-OnlineStockSpan.cpp
@@ -0,0 +1,25 @@
+// Last updated: 6/26/2026, 1:42:11 PM
+class StockSpanner {
+public:
+    stack<pair<int,int>>st;
+
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span =1;
+        while(!st.empty()&&st.top().first<=price){
+            span+=st.top().second;
+            st.pop();
+        }    
+        st.push({price,span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
