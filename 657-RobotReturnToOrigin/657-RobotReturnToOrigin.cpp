7ba031220fb7dacb02d02906cb6adebc58d1769f
NishadDamale <nishaddamale1010@gmail.com>
2026-06-26 13:42:27 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/26/2026, 1:42:29 PM
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+
+        for (char move : moves) {
+            if (move == 'R') x++;
+            else if (move == 'L') x--;
+            else if (move == 'U') y++;
+            else if (move == 'D') y--;
+        }
+
+        return x == 0 && y == 0;
+    }
+};
